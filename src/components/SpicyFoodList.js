921651286d1d3af74da5ef71b3441f30d3b2d577
Yifan <datasciencewyfan@gmail.com>
2022-06-08 21:08:16 -0700

    commit tree 07a95080bf8fb79b1d40e18a1cab2178b51c4e31

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -7,19 +7,55 @@ function SpicyFoodList() {
   function handleAddFood() {
     const newFood = getNewSpicyFood();
     console.log(newFood);
+    setFoods([ ...foods , newFood])
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  const [genre , setGenre] = useState('All')
+
+  function changeGenre(e) { 
+    console.log(e.target.value)
+    setGenre(e.target.value)
+  }
+
+  function handleLiClick(id) {
+    setFoods(foods.map( food =>{
+      let newFood = {...food}      
+      if (food.id===id) {
+        newFood.heatLevel +=1
+      }
+      return newFood
+    }))
+  }
+
+  const foodList = foods.filter((food) => {
+    if (genre === 'All') {
+      return true
+    } else {
+      return food.cuisine === genre
+    }
+  }).map((food) => (
+    <li key={food.id} onClick ={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
-  return (
+  return (  
+    <>
+    <div>
+      <select name="filter" onChange={ (e) => changeGenre(e) }>
+      <option value="All">All</option>
+      <option value="American">American</option>
+      <option value="Sichuan">Sichuan</option>
+      <option value="Thai">Thai</option>
+      <option value="Mexican">Mexican</option>
+      </select>  
+    </div>
+    
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
+    </>
   );
 }
 
